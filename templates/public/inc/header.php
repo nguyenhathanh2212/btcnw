39616247fd732bba05bfe4095d0cdb46222bffd8
TDK <thanhtdk2212@gmail.com>
2018-05-20 11:52:45 +0700

    'tdk'

--- templates/public/inc/header.php
+++ templates/public/inc/header.php
@@ -5,21 +5,6 @@
     require_once $_SERVER['DOCUMENT_ROOT'].'/functions/session.php';
     require_once $_SERVER['DOCUMENT_ROOT'].'/functions/replace.php';
 ?>
-<?php
-    $_SESSION['arIcon'] = array(
-        1 => 'fa-paw',
-        2 => 'fa-car',
-        3 => 'fa-laptop',
-        4 => 'fa-home',
-        5 => 'fa-bicycle',
-        6 => 'fa-female',
-        7 => 'fa-user',
-        8 => 'fa-cutlery',
-        9 => 'fa-shower',
-        10 => 'fa-gift',
-        11 => 'fa-star',
-    );
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -55,7 +40,7 @@
             <div class="fa fa-user-circle-o"></div>
             <div class="user-log">
                 <?php if ($session->has('UserAuthenticate')) { ?>
-                <a href="<?php echo '/timeline/' . convertUtf8ToLatin($session->get('UserAuthenticate')['username']) . '-' . $session->get('UserAuthenticate')['id_user'] ?>">
+                <a href="<?php echo '/timeline/' . convertUtf8ToLatin($session->get('UserAuthenticate')['username']) . '-' . $session->get('UserAuthenticate')['id'] ?>">
                     <?php echo $session->get('UserAuthenticate')['username'] ?>
                 </a> | 
                 <a href="#" id="logout-btn">Đăng xuất</a>
@@ -105,18 +90,18 @@
         </div>
     </div>
     <div class="tail-header">
-            <ul>
-                <li><a href="/" class="fa fa-home"></a></li>
-                <li><a href="/category/vat-nuoi-1" class="fa fa-paw"></span> Vật nuôi</a></li>
-                <li><a href="/category/xe-co-2" class="fa fa-car"> Xe cộ</a></li>
-                <li><a href="/category/dien-tu-3" class="fa fa-laptop"> Điện tử</a></li>
-                <li><a href="/category/bat-dong-san-4" class="fa fa-home"> Bất động sản</a></li>
-                <li><a href="/category/the-thao-5" class="fa fa-bicycle"> Thể thao</a></li>
-                <li><a href="/category/thoi-trang-6" class="fa fa-female"> Thời trang</a></li>
-                <li><a href="/category/tuyen-dung-7" class="fa fa-user"> Tuyển dụng</a></li>
-                <li><a href="/category/thuc-pham-8" class="fa fa-cutlery"> Thực phẩm</a></li>
-                <li><a href="/category/noi-ngoai-that-9" class="fa fa-shower"> Nội ngoại thất</a></li>
-                <li><a href="/category/khac-10" class="fa fa-gift"> Khác</a></li>
-            </ul>
+        <ul>
+            <li><a href="/" class="fa fa-home"></a></li>
+            <li><a href="/category/vat-nuoi-1" class="fa fa-paw"></span> Bác sĩ</a></li>
+            <li><a href="/category/xe-co-2" class="fa fa-car"> Bán hàng</a></li>
+            <li><a href="/category/dien-tu-3" class="fa fa-laptop"> Bảo hiểm</a></li>
+            <li><a href="/category/bat-dong-san-4" class="fa fa-home"> Bất động sản</a></li>
+            <li><a href="/category/the-thao-5" class="fa fa-bicycle"> Phiên dịch</a></li>
+            <li><a href="/category/thoi-trang-6" class="fa fa-female"> Cơ khí</a></li>
+            <li><a href="/category/tuyen-dung-7" class="fa fa-user"> Dệt may, giày da</a></li>
+            <li><a href="/category/thuc-pham-8" class="fa fa-cutlery"> Dược sĩ</a></li>
+            <li><a href="/category/noi-ngoai-that-9" class="fa fa-shower"> Công nghệ thông tin</a></li>
+            <li><a href="/category/khac-10" class="fa fa-gift"> Khác</a></li>
+        </ul>
     </div>
     <!--end header-->
